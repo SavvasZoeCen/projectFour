4ef446424d58a9a4f92c7d26f8991660c975756f
SavvasZoeCen <74330326+SavvasZoeCen@users.noreply.github.com>
2021-07-25 17:24:30 -0700

    v0c7

--- database_endpoint.py
+++ database_endpoint.py
@@ -19,12 +19,16 @@ app = Flask(__name__)
 #These decorators allow you to use g.session to access the database inside the request code
 @app.before_request
 def create_session():
+    print(">>>create_session")
     g.session = scoped_session(DBSession) #g is an "application global" https://flask.palletsprojects.com/en/1.1.x/api/#application-globals
+    print("create_session>>>")
 
 @app.teardown_appcontext
 def shutdown_session(response_or_exc):
+    print(">>>shutdown_session")
     g.session.commit()
     g.session.remove()
+    print(">>>shutdown_session")
 
 """
 -------- Helper methods (feel free to add your own!) -------
@@ -32,10 +36,12 @@ def shutdown_session(response_or_exc):
 
 def log_message(d)
     # Takes input dictionary d and writes it to the Log table
+    print(">>>log_message")
     msg = json.dumps(d)
     log = Log(message = msg)
     g.session.add(log)
     g.session.commit()
+    print("log_message>>>")
 
 """
 ---------------- Endpoints ----------------
@@ -43,6 +49,7 @@ def log_message(d)
     
 @app.route('/trade', methods=['POST'])
 def trade():
+    print(">>>trade")
     if request.method == "POST":
         content = request.get_json(silent=True)
         print( ">>>>>>>>>>>>>>>>>>>>>", f"content = {json.dumps(content)}" )
@@ -100,9 +107,11 @@ def trade():
 def order_book():
     #Your code here
     #Note that you can access the database session using g.session
+    print(">>>order_book")
     orders = g.session.query(Order).options(laod_only("sender_pk", "receiver_pk", "buy_currency", "sell_currency", "buy_amount", "sell_amount", "signature")).all()
     print("~~~~~~~~~~~~~", orders)
     result = {'data': orders}
+    print(">>>order_book")
     return jsonify(result)
 
 if __name__ == '__main__':
